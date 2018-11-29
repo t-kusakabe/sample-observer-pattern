eedfd421b06be2f8c3f4a211a886d61fa05e4317
t-kusakabe <ttmac0704@gmail.com>
2018-11-29 18:09:25 +0900

    雑にmodel作る

---
+++ lib/model/Payroll.rb
@@ -0,0 +1,5 @@
+class Payroll
+  def update(changed_employee)
+    puts "His salary is #{changed_employee.salary}. #{changed_employee.title} will cut off a new check for me."
+  end
+end
---
+++ lib/model/TaxMan.rb
@@ -0,0 +1,5 @@
+class TaxMan
+  def update(changed_employee)
+    puts "send #{changed_employee.name} a new tax invoice."
+  end
+end
